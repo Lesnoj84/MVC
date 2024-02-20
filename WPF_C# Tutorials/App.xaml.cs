5b781cfb9f90d6c09f83bf8741bdd915bb72828c
Vitalij <Vitalij@DESKTOP-KACBSPL>
2024-02-20 11:08:34 +0100

    Add project files.

---
+++ WPF_C# Tutorials/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_C__Tutorials
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
