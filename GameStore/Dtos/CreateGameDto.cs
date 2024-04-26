0f0c341d8a1dcfe9629d0800c87720d3170e9583
Lesnoj84 <crasherc@seznam.cz>
2024-04-27 00:22:23 +0200

    Bulky Project

---
+++ GameStore/Dtos/CreateGameDto.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStore.Api.Dtos
+{
+    public record CreateGameDto(
+    //int Id,on Id because the Id will be provided by Database.
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate);
+    
+}
--- GameStore/Program.cs
+++ GameStore/Program.cs
@@ -1,6 +1,30 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+const string GetGameEndpointName = "GetGame";
+
+List<GameDto> games = [
+        new(1,"Street Fighter","Fighting",19.9M,new DateOnly(1992,3,15)),
+        new(2,"Fifa","Sport",24.9M, new DateOnly(1993,7,11)),
+        new(3,"Sonic","Arcade",21.9M,new DateOnly(1991,8,25)),
+        new(4,"Contra","Shooter",25.9M,new DateOnly(1990, 10, 5))
+        ];
+
+//GET / games
+app.MapGet("games",()=>games);
+
+//GET / games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id))
+    .WithName("GetGameEndpointName");
+
+//POST / games
+app.MapPost("games",(CreateGameDto newGame) =>
+{
+    GameDto game = new(games.Count + 1, newGame.Name, newGame.Genre, newGame.Price, newGame.ReleaseDate);
+    games.Add(game);
+    return Results.CreatedAtRoute("GetGameEndpointName", new { id=game.Id},game);
+});
 
 app.Run();
