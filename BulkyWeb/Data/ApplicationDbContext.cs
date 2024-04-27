53f8fdd32b8b889a35b5c2553c49f0be1879effc
Lesnoj84 <crasherc@seznam.cz>
2024-04-28 00:34:51 +0200

    Category List + bootstrip + bootwatch+buttons

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Parameter for the database and to make database 
+        public DbSet<Category> Categories { get; set; }
+
+        //Method that is provided by Db entityFrameWork to fill manually the databse
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 });
+        }
+       
+    }
+
+}
+
+
