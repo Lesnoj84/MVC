f8f6177094ba483e9161e9bd8ecbe1677f98a36d
Lesnoj84 <74045889+Lesnoj84@users.noreply.github.com>
2024-05-30 09:24:19 +0200

    Delete KampaPlays_MVVM directory

--- KampaPlays_MVVM/Model/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KampaPlays_MVVM.Model
-{
-    public class Item
-    {
-        public string ItemName { get; set; }
-        public string SerialNumber { get; set; }
-        public int Quantity { get; set; }
-    }
-}
